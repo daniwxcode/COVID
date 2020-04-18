2262a13378d63fec2278d74f02544ed59b1eeddb
Daniel WOAGOU <daniwx@gmail.com>
2020-04-18 12:57:42 +0000

    Ajout de l'icone sur Android

--- COVID.Android/MainActivity.cs
+++ COVID.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Plugin.CurrentActivity;
 
 namespace COVID.Droid
 {
-    [Activity(Label = "COVID", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "COVID", Icon = "@mipmap/ic_Covid", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
