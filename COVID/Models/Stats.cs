ab98ca8a648cb0c9ac9dec8d6c47b422f16c4183
Daniel WOAGOU <daniwx@gmail.com>
2020-04-29 11:18:16 +0000

    Version one

---
+++ COVID/Models/Stats.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COVID.Models
+{
+    public class Stats
+    {
+        public uint ActiveCases { get; set; }
+        public uint Cured { get; set; }
+        public uint Deaths { get; set; }
+        public uint Total { get { return ActiveCases + Cured + Deaths; } }
+
+        public string TimeInfo{get;set;}
+        public override string ToString()
+        {
+            return $"\n Actifs= {ActiveCases} -Guéris={Cured} - Décès ={Deaths} - Total ={Total}";
+        }
+    }
+}
---
+++ COVID/Services/Covid19TgService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AngleSharp;
+using COVID.Models;
+using Xamarin.Essentials;
+
+namespace COVID.Services
+{
+    public static class Covid19TgService
+    {
+        public static void AppelNumeroVert()
+        {
+            PhoneDialer.Open(Covid19TgService.NumeroVert);
+        }
+        public static string NumeroVert { get; set; } = "111";
+        public static List<Details> InfosCovid { get; set; }
+        public static async Task<List<Details>> GetDetailsAsync()
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync("https://covid19.gouv.tg/situation-au-togo/");
+            var details = new List<Details>();
+
+            var sections = document.QuerySelectorAll(".ee-loop__item>article>div>div>div");
+            string xt = string.Empty;
+
+            foreach (var item in sections.Skip(1))
+            {
+                var itemDetails = new Details();
+                xt += "\n\n";
+                var itemsections = item.QuerySelectorAll("section");
+                var itemHtmlDetails = itemsections.FirstOrDefault().QuerySelectorAll("h2");
+
+
+                int i = 0;
+                Stats itemStats = new Stats();
+                itemStats.TimeInfo = $"{itemHtmlDetails[0].InnerHtml} à { itemHtmlDetails[1].InnerHtml}";
+                itemStats.ActiveCases = itemHtmlDetails[3].InnerHtml.GetInt();
+                itemStats.Cured = itemHtmlDetails[4].InnerHtml.GetInt();
+                itemStats.Deaths = itemHtmlDetails[5].InnerHtml.GetInt();
+                itemDetails.Stat = itemStats;
+
+                itemDetails.Histoire = Regex.Replace(itemsections[1].TextContent, @"^[\s\t\n]+|[\s\t\n]+$", "");
+                itemDetails.Histoire = Regex.Replace(itemDetails.Histoire, @"<[^>]+>|&nbsp;", "").Trim();
+                itemDetails.Histoire = Regex.Replace(itemDetails.Histoire, @"\s{2,}", " "); ;
+                details.Add(itemDetails);
+
+            }
+
+            return details;
+        }
+
+        //public static async Task<List<Details>> GetDetailsAsync()
+        //{
+        //    var config = Configuration.Default.WithDefaultLoader();
+        //    var context = BrowsingContext.New(config);
+        //    var document = await context.OpenAsync("http://covid19.gouv.tg/situation-au-togo/");
+        //    var details = new List<Details>();
+
+        //    var sections = document.QuerySelectorAll(".ee-loop__item>article>div>div>div");
+        //    string xt = string.Empty;
+
+        //    foreach (var item in sections.Skip(1))
+        //    {
+        //        var itemDetails = new Details();
+        //        xt += "\n\n";
+        //        var itemsections = item.QuerySelectorAll("section");
+        //        var itemHtmlDetails = itemsections.FirstOrDefault().QuerySelectorAll("h2");
+        //        itemDetails.Date = $"{itemHtmlDetails[0].InnerHtml} à { itemHtmlDetails[1].InnerHtml}";
+
+
+        //        Stats itemStats = new Stats();
+        //        itemStats.ActiveCases = itemHtmlDetails[3].InnerHtml.GetInt();
+        //        itemStats.Cured = itemHtmlDetails[4].InnerHtml.GetInt();
+        //        itemStats.Deaths = itemHtmlDetails[5].InnerHtml.GetInt();
+        //        itemDetails.Stat = itemStats;
+        //        foreach (var history in itemsections[1].QuerySelectorAll("p"))
+        //        {
+        //            itemDetails.Histoire += $"\n{history.InnerHtml}";
+        //        }
+        //        details.Add(itemDetails);
+
+        //    }
+
+        //    return details;
+        //}
+
+    }
+}
